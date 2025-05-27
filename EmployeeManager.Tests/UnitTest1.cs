458b7566be41edb117cf6beb3b43378d13a5a4a5
edine-noella <edinenoella@gmail.com>
2025-05-27 18:56:50 +0200

    setup tests with NUnit

---
+++ EmployeeManager.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+namespace EmployeeManager.EmployeeManager.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
